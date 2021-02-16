182e52479de74e8f5aa378a150283b2f5fae0a52
VanderWaals <1450393960@qq.com>
2021-02-16 22:05:52 +0800

    FAT12

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -4,7 +4,7 @@
 %ifdef _BOOT_DEBUG_
     org 0100h               ; 调试状态，做成.COM文件，可调试
 %else
-    org 0x7c00h             ; Boot状态，Bios将把 Boot Sector 加载到0:7c00处执行
+    org 7c00h             ; Boot状态，Bios将把 Boot Sector 加载到0:7c00处执行
 %endif
 ;=====================================================================================
 %ifdef _BOOT_DEBUG_
@@ -15,14 +15,52 @@
 ;======================================================================================
 
     jmp short LABEL_START   ;跳转到程序开始处
-    nop
+    nop                     ;不可省略
+
+    BS_OEMName  DB 'Typedef_'   ; OEM String, 必须 8 个字节
+
+    BPB_BytsPerSec  DW 512      ; 每扇区字节数
+
+    BPB_SecPerClus  DB 1        ; 每簇多少扇区
+
+    BPB_RsvdSecCnt  DW 1        ; Boot 记录占用多少扇区
+
+    BPB_NumFATs DB 2            ; 共有多少 FAT 表
+
+    BPB_RootEntCnt  DW 224      ; 根目录文件数最大值
+
+    BPB_TotSec16    DW 2880     ; 逻辑扇区总数
+
+    BPB_Media   DB 0xF0         ; 媒体描述符
+
+    BPB_FATSz16 DW 9            ; 每FAT扇区数
+
+    BPB_SecPerTrk   DW 18       ; 每磁道扇区数
+
+    BPB_NumHeads    DW 2        ; 磁头数(面数)
+
+    BPB_HiddSec DD 0            ; 隐藏扇区数
+
+    BPB_TotSec32    DD 0        ; 如果 wTotalSectorCount 是 0 由这个值记录扇区数
+
+    BS_DrvNum   DB 0            ; 中断 13 的驱动器号
+
+    BS_Reserved1    DB 0        ; 未使用
+
+    BS_BootSig  DB 29h          ; 扩展引导标记 (29h)
+
+    BS_VolID    DD 0            ; 卷序列号
+
+    BS_VolLab   DB 'FlyanxOS'   ; 卷标, 必须 11 个字节
+
+    BS_FileSysType  DB 'FAT12'   ; 文件系统类型, 必须 8个字节
 
 ;======================================================================================
 LABEL_START:
         mov ax,cs
         mov ds,ax
         mov ss,ax
-        mov sp,StackBase
+        mov sp,BaseOfStack
 
         ;清屏，清理BIOS输出
         mov ax,0x0600   ;AH=6,AL=0h
@@ -33,7 +71,7 @@ LABEL_START:
 
         ;显示字符串 "Booting......"
         mov dh,0        ;"Booting......"
-        call DisStr     ;显示字符串
+        call DispStr     ;显示字符串
 
         ;死循环
         jmp $
