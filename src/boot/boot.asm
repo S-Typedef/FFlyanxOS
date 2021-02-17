b982adeb507684b9f632a63fdd28f35a794a01d8
VanderWaals <1450393960@qq.com>
2021-02-17 13:15:30 +0800

    04.5

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -2,10 +2,15 @@
 ;====================================================================================
 ;%define   _BOOT_DEBUG_     ;做 BOOT Sector 时一定将此行注释掉！此行打开后用 nasm Boot.asm -o Boot.com 做成一个.COM文件易于调试
 %ifdef _BOOT_DEBUG_
-    org 0100h               ; 调试状态，做成.COM文件，可调试
+    org 0x100               ; 调试状态，做成.COM文件，可调试
 %else
     org 7c00h             ; Boot状态，Bios将把 Boot Sector 加载到0:7c00处执行
 %endif
+
+;LOADER加载的段地址
+LOADER_SEG      equ 0x9000
+LOADER_OFFSET   equ 0x100
+
 ;=====================================================================================
 %ifdef _BOOT_DEBUG_
     BaseOfStack equ 0100h   ;调试状态
@@ -16,45 +21,8 @@
 
     jmp short LABEL_START   ;跳转到程序开始处
     nop                     ;不可省略
-
-    BS_OEMName  DB 'Typedef_'   ; OEM String, 必须 8 个字节
-
-    BPB_BytsPerSec  DW 512      ; 每扇区字节数
-
-    BPB_SecPerClus  DB 1        ; 每簇多少扇区
-
-    BPB_RsvdSecCnt  DW 1        ; Boot 记录占用多少扇区
-
-    BPB_NumFATs DB 2            ; 共有多少 FAT 表
-
-    BPB_RootEntCnt  DW 224      ; 根目录文件数最大值
-
-    BPB_TotSec16    DW 2880     ; 逻辑扇区总数
-
-    BPB_Media   DB 0xF0         ; 媒体描述符
-
-    BPB_FATSz16 DW 9            ; 每FAT扇区数
-
-    BPB_SecPerTrk   DW 18       ; 每磁道扇区数
-
-    BPB_NumHeads    DW 2        ; 磁头数(面数)
-
-    BPB_HiddSec DD 0            ; 隐藏扇区数
-
-    BPB_TotSec32    DD 0        ; 如果 wTotalSectorCount 是 0 由这个值记录扇区数
-
-    BS_DrvNum   DB 0            ; 中断 13 的驱动器号
-
-    BS_Reserved1    DB 0        ; 未使用
-
-    BS_BootSig  DB 29h          ; 扩展引导标记 (29h)
-
-    BS_VolID    DD 0            ; 卷序列号
-
-    BS_VolLab   DB 'FlyanxOS'   ; 卷标, 必须 11 个字节
-
-    BS_FileSysType  DB 'FAT12'   ; 文件系统类型, 必须 8个字节
-
+    ;导入FAT12头及相关常量信息
+    %include "fat12hdr.inc"
 ;======================================================================================
 LABEL_START:
         mov ax,cs
---
+++ src/boot/loader.asm
@@ -0,0 +1,44 @@
+org 0x100
+    jmp start
+    BaseOfStack equ 0x100   ;调试状态
+
+start:
+    mov ax,cs
+    mov ds,ax
+    mov ss,ax
+    mov sp,BaseOfStack
+
+    ;显示字符串 "Hello LOader!"
+    mov dh,0        ;"Hello LOader!"
+    call DispStr     ;显示字符串
+
+    ;死循环
+    jmp $
+
+
+    ;-----------------------------------------------------------------------
+    ;要显示字符串
+    LoaderFileName db "LODAER BIN", 0   ;LOADER.BIN之文件名
+    ;简化代码，每个字符串长度均为 MessageLength
+    MessageLength       equ 13
+    Message:    db "Hello LOader!"      ;13字节，不够用空格补齐，序号0
+    ;=======================================================================
+    ;-----------------------------------------------------------------------
+    ;函数名：DispStr
+    ;-----------------------------------------------------------------------
+    ; 作用:
+    ;   显示一个字符串，函数开始时， dh中应该是字符串序号（0-based）
+    DispStr:
+        mov ax,MessageLength
+        mul dh
+        add ax,Message
+        ;ES:BP = 串地址
+        mov bp,ax
+        mov ax,ds
+        mov es,ax
+        mov cx,MessageLength    ; CS =  串长度
+        mov ax,01301h           ; AH = 13 ,AL = 01h
+        mov bx,0007h            ;页号为0(BH=0) 黑底白字(BL = 07h)
+        mov dl,0
+        int 10h
+        ret
