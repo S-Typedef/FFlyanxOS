c1ad4073c707700642a0828b4f25a7bf0311afa0
VanderWaals <1450393960@qq.com>
2021-02-19 23:37:02 +0800

    10

--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -5,10 +5,27 @@ org 0x100
 ;=======================================================================
 ;   导入头文件
 ;-----------------------------------------------------------------------
-%include    "load.inc"         ;挂载点相关信息
-%include    "fat12hdr.inc"  ;需要加载内核文件
-
-BaseOfStack equ 0x100       ;调试状态
+%include    "load.inc"          ;挂载点相关信息
+%include    "fat12hdr.inc"      ;需要加载内核文件
+%include    "pm.inc"            ;保护模式信息，各种宏和变量
+;============================================================================
+;   GDT全局描述符表相关信息以及堆栈信息
+;----------------------------------------------------------------------------
+; 描述符                        基地址        段界限       段属性
+LABEL_GDT:			Descriptor	0,          0,          0							; 空描述符，必须存在，不然CPU无法识别GDT
+LABEL_DESC_CODE:	Descriptor	0,          0xfffff,    DA_32 | DA_CR | DA_LIMIT_4K	; 0~4G，32位可读代码段，粒度为4KB
+LABEL_DESC_DATA:    Descriptor  0,          0xfffff,    DA_32 | DA_DRW | DA_LIMIT_4K; 0~4G，32位可读写数据段，粒度为4KB
+LABEL_DESC_VIDEO:   Descriptor  0xb8000,    0xfffff,    DA_DRW | DA_DPL3            ; 视频段，特权级3（用户特权级）
+; GDT全局描述符表 -------------------------------------------------------------
+GDTLen              equ $ - LABEL_GDT                           ; GDT的长度
+GDTPtr              dw GDTLen - 1                               ; GDT指针.段界限
+                    dd LOADER_PHY_ADDR + LABEL_GDT              ; GDT指针.基地址
+; GDT选择子 ------------------------------------------------------------------
+SelectorCode        equ LABEL_DESC_CODE - LABEL_GDT             ; 代码段选择子
+SelectorData        equ LABEL_DESC_DATA - LABEL_GDT             ; 数据段选择子
+SelectorVideo       equ LABEL_DESC_VIDEO - LABEL_GDT | SA_RPL3  ; 视频段选择子，特权级3（用户特权级）
+; GDT选择子 ------------------------------------------------------------------
+BaseOfStack	        equ	0x100                                   ; 基栈
 ;=======================================================================
 ;   16位实模式代码段
 ;-----------------------------------------------------------------------
@@ -42,7 +59,7 @@ MemChkLoop:
     jmp MemChkLoop
 MemChkFail:
     mov dword [_ddMCRCount],0     ;检查失败，ARDS数量设置为0
-    mov dh,1
+    mov dh,2
     call DispStr            ;检查失败打印"Mem Chk Fail!"
 
     ;死循环
@@ -114,6 +131,19 @@ FILENAME_FOUND:
     ;准备参数读书文件数据扇区
     mov ax,RootDirSectors   ;ax=根目录占用空间
     and di,0xfff0
+
+    push eax
+    mov eax,[es:di+0x1c]
+    mov dword   [dwKernelSize],eax
+    cmp eax,KERNEL_HAVE_SPACE
+    ja KERNEL_FILE_TOO_LARGE
+    pop eax
+    jmp FILE_START_LAOD
+KERNEL_FILE_TOO_LARGE:
+    mov dh,3
+    call DispStr
+    jmp $
+FILE_START_LOAD:
     add di,0x1a             ;FAT目录项0x1a处是文件数据所在的第一个簇号
     mov cx,[es:di]          ;cx=文件数据所在的第一个簇号
     push cx
@@ -150,28 +180,62 @@ LOADING_FILE:
     add ax,dx
     add ax,DeltaSectorNo        ;簇号+根目录占据空间+文件开始扇区号 == 文件数据的第一个扇区
     add bx,[BPB_BytsPerSec]     ;bx+=扇区字节量
+    ja KERNEL_GREAT_64KB
+    jmp CONTINUE_LOADING
+KERNEL_GREAT_64KB:
+    push ax
+    mov ax,es
+    add ax,0x1000
+    mov es,ax
+    pop ax
+CONTINUE_LOADING:
     jmp LOADING_FILE
 FILE_LOADED:
-    mov dh,3
-    call DispStr                ;加载成功
-    ;死循环
+    call KillMotor
+    mov dh,1
+    call DispStr    ; 打印"KERNEL OK!"
+;----------------------------------------------------------------------------
+; 准备进入32位保护模式
+;----------------------------------------------------------------------------
+    ; 1 首先，进入保护模式必须有 GDT 全局描述符表，我们加载 gdtr（gdt地址指针）
+    lgdt	[GDTPtr]
+
+    ; 2 由于保护模式中断处理的方式和实模式不一样，所以我们需要先关闭中断，否则会引发错误
+    cli
+
+    ; 3 打开地址线A20，不打开也可以进入保护模式，但内存寻址能力受限（1MB）
+    in al, 92h
+    or al, 00000010b
+    out 92h, al
+
+    ; 4 进入16位保护模式，设置cr0的第0位：PE（保护模式标志）为1
+    mov eax, cr0
+    or 	eax, 1
+    mov cr0, eax
+
+    ; 5 真正进32位入保护模式！前面的4步已经进入了保护模式
+    ; 	现在只需要跳入到一个32位代码段就可以真正进入32位保护模式了！
+    jmp dword SelectorCode:LOADER_PHY_ADDR + PM_32_START
+
+    ; 如果上面一切顺利，这一行永远不可能执行的到
     jmp $
-
-;=========================================================
-;变量
-wRootDirSizeLoop    dw      RootDirSectors  ;根目录占用的扇区数，在循环中将被递减至0
-wSector             dw      0               ;要读取的扇区号
-isOdd               db      0               ;FAT条目是否奇数
-
-;-----------------------------------------------------------------------
-;要显示字符串
+;============================================================================
+; 变量
+wRootDirSizeLoop    dw      RootDirSectors  ; 根目录占用的扇区数，在循环中将被被逐步递减至0
+wSector             dw      0               ; 要读取的扇区号
+isOdd               db      0               ; 读取的FAT条目是不是奇数项?
+dwKernelSize        dd      0               ; 内核文件的大小
+;============================================================================
+; 要显示的字符串
+;----------------------------------------------------------------------------
 KernelFileName  db      "KERNEL  BIN", 0   ;KERNEL.BIN之文件名
 ;简化代码，每个字符串长度均为 MessageLength
 MessageLength   equ 10
-Message:        db      "Loading..."      ;10字节，不够用空格补齐，序号0
-                db      "MemChkFail"
-                db      "No KERNEL!"
-                db      "Hello KER!"
+Message:        db      "Loading..."        ;10字节，不够用空格补齐，序号0
+                db      "KERNEL OK!"        ;1
+                db      "MemChkFail"        ;2
+                db      "Too large!"        ;3
+                db      "NO KERNEL!"        ;4
 ;=======================================================================
 ;-----------------------------------------------------------------------
 ;函数名：DispStr
@@ -287,13 +351,47 @@ GET_FATEntry_OK:
     pop es
     ret
 
-
+;============================================================================
+;----------------------------------------------------------------------------
+; 函数名: KillMotor
+;----------------------------------------------------------------------------
+; 作用:
+;	关闭软驱马达，有时候软驱读取完如果不关闭马达，马达会持续运行且发出声音
+KillMotor:
+    push	dx
+    mov	dx, 03F2h
+    mov	al, 0
+    out	dx, al
+    pop	dx
+    ret
 ;=======================================================================
 ;   32位数据段
 ;-----------------------------------------------------------------------
-[section .data32]
+[section .code32]
 align 32
-DATA32:
+[bits 32]
+PM_32_START:            ; 跳转到这里，说明已经进入32位保护模式
+    mov ax, SelectorData
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov ss, ax              ; ds = es = fs = ss = 数据段
+    mov esp, TopOfStack     ; 设置栈顶
+    mov ax, SelectorVideo
+    mov gs, ax              ; gs = 视频段
+
+    ; 打印一些字符，自己操作显存，将字符写入到显存中
+    ; 显示 "PM" 在第 9 行第 0 列
+    mov edi, (80 * 9 + 0) * 2	; 屏幕第 9 行，第 0 列。
+    mov ah, 0xC                 ; 0000：黑底	1100：红字
+    mov al, 'P'
+    mov word [gs:edi], ax       ; 将'P'写入屏幕第 9 行，第 0 列。
+    add edi , 2                 ; edi + 2，指向下一列
+    mov al, 'M'
+    mov word [gs:edi], ax       ; 将'M'写入屏幕第 9 行，第 1 列。
+
+    ; 死循环
+    jmp $
 ;=======================================================================
 ;   16位实模式下的数据地址符号
 ;-----------------------------------------------------------------------
@@ -308,11 +406,13 @@ _ARDS:
     _ddType             dd      0       ;ARDS类型，判断是否可以被OS使用
 ;内存检查结果缓冲区，用于存放内存检查的ARDS结构，256字节对齐32位
 ; 256/32 == 12.8,可以存放12个ARDS
-_MemChkBuf  times 256   db      0       ;
+_MemChkBuf  times 256   db      0
 
-
-;=======================================================================
+;----------------------------------------------------------------------------
 ;   32位模式下的数据地址符号
-;-----------------------------------------------------------------------
+;----------------------------------------------------------------------------
 
-;=======================================================================
+; 堆栈就在数据段的末尾，一共给这个32位代码段堆栈分配4KB
+StackSpace: times 0x1000    db 0
+TopOfStack  equ LOADER_PHY_ADDR + $     ; 栈顶
+;============================================================================
