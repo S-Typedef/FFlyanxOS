0d3ce84c6f21f0a92ef3aaff6018d595de1c6f1e
VanderWaals <1450393960@qq.com>
2021-02-20 20:53:55 +0800

    16

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,18 +1,17 @@
-/* Copyright (C) 2007 Free Software Foundation, Inc.
+/* Copyright (C) 2007 Free Software Foundation, Inc. 
  * See the copyright notice in the file /usr/LICENSE.
  * Created by flyan on 2020/2/20.
  * QQ: 1341662010
  * QQ-Group:909830414
  * gitee: https://gitee.com/flyanh/
  *
- * ¸ÃÎÄ¼ş°üº¬flyanxµÄÖ÷³ÌĞò¡£
+ * è¯¥æ–‡ä»¶åŒ…å«flyanxçš„ä¸»ç¨‹åºã€‚
  *
- * ¸ÃÎÄ¼şµÄÈë¿ÚµãÊÇ£º
- *  - flyanx_main:      flyanxµÄÖ÷³ÌĞò
+ * è¯¥æ–‡ä»¶çš„å…¥å£ç‚¹æ˜¯ï¼š
+ *  - flyanx_main:      flyanxçš„ä¸»ç¨‹åº
  */
 
-int display_position = (80 * 6 + 0) * 2;     // ´ÓµÚ 6 ĞĞµÚ 0 ÁĞ¿ªÊ¼ÏÔÊ¾
-void low_print(char* str);
+#include "kernel.h"
 
 void flyanx_main(void){
 
